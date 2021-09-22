bf510d048f3ec9a60b493836227e4406c3669731
Renan <souza.rnn@gmail.com>
2021-09-22 11:23:43 -0300

    Integrating the filter with the sales summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         <Filter onFilterChange={onFilterChange} />
         <SalesByDate filterData={filterData} />
         <div className="sales-overview-container">
-          <SalesSummary />
+          <SalesSummary filterData={filterData} />
           <PieChartCard 
             name="Lojas" 
             labels={['Uberlândia', 'Araguari', 'Uberaba']} 
